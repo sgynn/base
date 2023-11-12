255f82b49436ab5b1e3f260328ae1b79faaff502
Sam Gynn <dragoninspire@gmail.com>
2023-11-12 17:14:23 +0000

    PNG loader can load images from memory

--- include/base/png.h
+++ include/base/png.h
@@ -1,5 +1,4 @@
-#ifndef _BASE_PNG_
-#define _BASE_PNG_
+#pragma once
 
 namespace base {
 	class PNG {
@@ -7,19 +6,19 @@ namespace base {
 		PNG() : data(0), width(0), height(0), bpp(0) {}
 		PNG(const PNG&);
 		~PNG() { clear(); }
-		const PNG& operator=(const PNG&);
-		
-		// c++11 move functions
 		PNG(PNG&& p);
+		const PNG& operator=(const PNG&);
 		const PNG& operator=(PNG&&);
 		
-		/** Load the bitmap image - returns image object or NULL */
+		/// Load a png image from file
 		static PNG load(const char* filename);
-		/** Create a blank image */
+		/// Load a png image from data held in memory
+		static PNG loadEmbedded(unsigned size, const char* data);
+		/// Create a new image from pixel data
 		static PNG create(int width, int height, int depth, const char* data=0);
-		/** Save image to file*/
+		/// Save image to file
 		int save(const char* filename);
-		/** Clear png data */
+		// Clear png data
 		void clear();
 		
 		// Image data
@@ -29,4 +28,3 @@ namespace base {
 	};
 };
 
-#endif
--- src/png.cpp
+++ src/png.cpp
@@ -38,16 +38,121 @@ const PNG& PNG::operator=(PNG&& p) {
 	return *this;
 }
 
-/*
-//Custom read handler for PNG library so that it uses our File object.
-void PNGAPI png_read_data(png_structp png_ptr, png_bytep data, png_size_t length) {
-	png_size_t check;
-	if(png_ptr == NULL) return;
-	FILE *fp = (FILE*)png_ptr->io_ptr;
-	check = (png_size_t) fread((char*)data, 1, length, fp); 
-	if(check != length) png_error(png_ptr, "Read Error");
+struct PNGMemoryReader { const char* data; size_t size; size_t read; };
+void png_read_memory(png_structp png_ptr, png_bytep data, png_size_t length) {
+	PNGMemoryReader* src = (PNGMemoryReader*)png_get_io_ptr(png_ptr);
+	if(!src) return;
+	if(src->read + length > src->size) return;
+	memcpy(data, src->data + src->read, length);
+	src->read += length;
 }
-*/
+
+PNG PNG::loadEmbedded(unsigned size, const char* data) {
+	if(!data || !size) return PNG();
+	
+	// check for valid magic number
+	if(!png_check_sig((const unsigned char*)data, 8)) {
+		printf("Invalid PNG image\n"); 
+		return PNG();
+	}
+	
+	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if(!png_ptr) return PNG();
+	
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if(!info_ptr) {
+		png_destroy_read_struct(&png_ptr, NULL, NULL);
+		return PNG();
+	}
+	
+	// initialize the setjmp for returning properly after a libpng error occured
+	if(setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return PNG();
+	}
+	
+	PNGMemoryReader source { data, size, 8 };
+	png_set_read_fn(png_ptr, &source, png_read_memory);
+	png_set_sig_bytes(png_ptr, 8); // We have already read the signiture
+	
+	png_read_info(png_ptr, info_ptr);
+	int mcdepth = png_get_bit_depth(png_ptr, info_ptr);
+	int color_type = png_get_color_type(png_ptr, info_ptr);
+	if(color_type == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
+	
+	// convert 1-2-4 bits grayscale images to 8 bits grayscale.
+	int bit_depth = 32;
+	if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) png_set_expand_gray_1_2_4_to_8(png_ptr);
+	if(png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
+	if(bit_depth == 16) png_set_strip_16(png_ptr);
+	else if(bit_depth < 8) png_set_packing(png_ptr);
+	
+	/* update info structure to apply transformations */
+	png_read_update_info(png_ptr, info_ptr);
+	
+	// retrieve updated information
+	unsigned int mwidth;
+	unsigned int mheight;
+	png_get_IHDR(png_ptr, info_ptr,
+		(png_uint_32*)(&mwidth),
+		(png_uint_32*)(&mheight),
+		&mcdepth, &color_type,
+		NULL, NULL, NULL);
+	
+	// get image format and components per pixel
+	int iFormat=0;
+	switch (color_type) {
+	case PNG_COLOR_TYPE_GRAY:
+		iFormat = 1;
+		mcdepth = 8;
+		break;
+	
+	case PNG_COLOR_TYPE_GRAY_ALPHA:
+		iFormat = 2;
+		mcdepth = 16;
+		break;
+	
+	case PNG_COLOR_TYPE_RGB:
+		iFormat = 3;
+		mcdepth = 24;
+		break;
+	
+	case PNG_COLOR_TYPE_RGB_ALPHA:
+		iFormat = 4;
+		mcdepth = 32;
+		break;
+	
+	default:
+		printf("Invalid PNG Format\n");
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return PNG();
+	}
+
+	// 16bpp format
+	if(bit_depth==16) {
+		iFormat *= 2;
+		mcdepth *= 2;
+	}
+	
+	// Read the image data
+	char* mdata = (char*)malloc(sizeof(unsigned char) * mwidth * mheight * iFormat);
+	png_bytep* row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * mheight);
+	for(unsigned i=0; i < mheight; ++i) {
+		row_pointers[i] = (png_bytep)(mdata + (i * mwidth * iFormat));
+	}
+	png_read_image(png_ptr, row_pointers);
+	png_read_end(png_ptr, info_ptr);
+	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+	free(row_pointers);
+	
+
+	PNG png;
+	png.data = mdata;
+	png.width = mwidth;
+	png.height = mheight;
+	png.bpp = mcdepth;
+	return png;
+}
 
 PNG PNG::load(const char* filename) {
 	png_byte magic[8];
