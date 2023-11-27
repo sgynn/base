391822a58ce9d1a748983b0867203c238443810f
Sam Gynn <dragoninspire@gmail.com>
2023-11-27 19:17:28 +0000

    Added some handy math functions

--- include/base/math.h
+++ include/base/math.h
@@ -1,5 +1,4 @@
-#ifndef _BASE_MATH_
-#define _BASE_MATH_
+#pragma once
 
 /** general math header - goes in base directory... */
 
@@ -21,6 +20,16 @@ typedef unsigned char       ubyte,  uint8;
 #include "quaternion.h"
 #include "bounds.h"
 
+// Some more handy math functions
+inline float randf() { return (float)rand() / (float)RAND_MAX; }
+inline float fsign(float v) { return v<0? -1: v>0? 1: 0; }
+inline float fsign(float v, float epsilon) { return v<0-epsilon? -1: v>epsilon? 1: 0; }
+inline float fclamp(float v, float low=0, float high=1) { return fmin(fmax(v, low), high); }
+inline float flerp(float a, float b, float t) { return a * (1-t) + b * t; }
+inline float fstep(float v, float target, float amount) {
+	if(target > v + amount) return v + amount;
+	if(target < v - amount) return v - amount;
+	return target;
+}
 
-#endif
 
