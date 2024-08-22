b12e9822e1806478451f1e1c00060448ea04e5d0
Sam Gynn <dragoninspire@gmail.com>
2024-08-22 18:49:41 +0100

    Removed zlib and libpng - replaced with miniz and custom png loader

--- include/base/png.h
+++ include/base/png.h
@@ -6,8 +6,8 @@ namespace base {
 	class PNG {
 		public:
 		static Image load(const char* file);
-		static Image parse(const void* data, unsigned size);
+		static Image parse(const char* data, unsigned size);
 		static bool save(const Image& image, const char* file);
 	};
-};
+}
 
--- src/png.cpp
+++ src/png.cpp
@@ -1,248 +1,312 @@
-#include "base/png.h"
-
-#include "png/png.h"
-#include <cstdlib>
+#include <base/png.h>
+#include <cstdio>
 #include <cstring>
 
-#include <cstdio>
+#define MINIZ_HEADER_FILE_ONLY
+#include "miniz.c"
 
 using namespace base;
+using byte = unsigned char;
+
+static constexpr unsigned char magic[8] = {137, 80, 78, 71, 13, 10, 26, 10};
+enum class PNGColour : byte { Lum=0, RGB=2, Indexed=3, LumAlpha=4, RGBA=6 };
+struct PNGChunk {
+	unsigned length;
+	char type[4];
+	const byte* data;
+};
+
+struct PNGHeader {
+	unsigned width;
+	unsigned height;
+	byte bitDepth;
+	PNGColour colourType;
+	byte compression;
+	byte filter;
+	byte interlace;
+};
 
 
-struct PNGMemoryReader { const char* data; size_t size; size_t read; };
-void png_read_memory(png_structp png_ptr, png_bytep data, png_size_t length) {
-	PNGMemoryReader* src = (PNGMemoryReader*)png_get_io_ptr(png_ptr);
-	if(!src) return;
-	if(src->read + length > src->size) return;
-	memcpy(data, src->data + src->read, length);
-	src->read += length;
+// Offset from left pixel
+static void readSubFilter(byte* data, unsigned length, int depth) {
+	int bpp = (depth+7) >> 3;
+	data += bpp;
+	for(unsigned i=bpp; i<length; ++i) {
+		*data = ((int)*data + (int)*(data-bpp)) & 0xff;
+		++data;
+	}
+}
+// Offset from pixel above
+static void readUpFilter(byte* data, unsigned length, const byte* prev) {
+	for(unsigned i=0; i<length; ++i) {
+		data[i] = (data[i] + prev[i]) & 0xff;
+	}
+}
+// Offset from average of upper and left pixels
+static void readAvgFilter(byte* data, unsigned length, int depth, const byte* prev) {
+	int bpp = (depth+7) >> 3;
+	for(int i=0; i<bpp; ++i) { // First pixel - only above
+		*data = ((int)*data + (int)*prev++ / 2) & 0xff;
+		++data;
+	}
+	for(unsigned i=1; i<length; ++i) {
+		*data = ((int)*data + ((int)*prev++ + *(data-bpp)) / 2) & 0xff;
+		++data;
+	}
+}
+// Complicated thing based on upper, upper left amd left pixels
+static void readPaethFilter(byte* data, unsigned length, int depth, const byte* prev) {
+	int bpp = (depth+7) >> 3;
+	const byte* end = data + length;
+	for(int i=0; i<bpp; ++i) {
+		*data = ((int)*prev++ + *data) & 0xff;
+		++data;
+	}
+	while(data<end) {
+		int c = *(prev - bpp);
+		int a = *(data - bpp);
+		int b = *prev++;
+		int p = b - c;
+		int pc = a - c;
+		int pa = abs(p);
+		int pb = abs(pc);
+		pc = abs(p + pc);
+		// Find predictor
+		if(pb < pa) pa = pb, a = b;
+		if(pc < pa) a = c;
+		// Calculate pixel
+		*data = (a + *data) & 0xff;
+		++data;
+	}
 }
 
-Image process_png_loader(png_structp png_ptr, png_infop info_ptr);
+template<class ReadFunc>
+Image readPNGFile(ReadFunc&& read) {
+	// Check signiture
+	const byte* signiture = read(8);
+	if(memcmp(signiture, magic, 8) != 0) {
+		return Image();
+	}
 
-Image PNG::parse(const void* data, unsigned size) {
-	if(!data || !size) return Image();
-	
-	// check for valid magic number
-	if(!png_check_sig((const unsigned char*)data, 8)) {
-		printf("Invalid PNG image\n"); 
-		return Image();
+	static auto parseUInt = [](const byte* data)->unsigned {
+		return data[0]<<24 | data[1] << 16 | data[2] << 8 | data[3];
+	};
+
+	auto readChunk = [&read](PNGChunk& chunk) {
+		chunk.length = parseUInt(read(4));
+		memcpy(chunk.type, read(4), 4);
+		chunk.data = chunk.length? read(chunk.length): nullptr;
+		// crc check - using crc32 from miniz
+		unsigned crc = parseUInt(read(4));
+		unsigned crc1 = crc32(0, (byte*)chunk.type, 4);
+		if(chunk.data) crc1 = crc32(crc1, chunk.data, chunk.length);
+		if(crc != crc1) printf("CRC check failed on block %.4s\n", chunk.type);
+	};
+
+	z_stream stream;
+	memset(&stream, 0, sizeof(stream));
+	if(inflateInit(&stream) != Z_OK) return Image();
+	byte* rowBuffer = nullptr;
+	unsigned rowSize = 0;
+	unsigned bitsPerPixel = 0;
+
+	PNGHeader header = {0,0,0,PNGColour::Lum,0,0,0};
+	PNGChunk chunk;
+	chunk.data = nullptr;
+	byte* pixels = nullptr;
+	byte* palette = nullptr;
+	Image::Format format = Image::INVALID;
+
+	readChunk(chunk);
+	while(chunk.type[0]) {
+		// do something with chunk
+		if(memcmp(chunk.type, "IHDR", 4)==0) {
+			header.width = parseUInt(chunk.data);
+			header.height = parseUInt(chunk.data+4);
+			header.bitDepth = chunk.data[8];
+			header.colourType = (PNGColour)chunk.data[9];
+			header.compression = chunk.data[10];
+			header.filter = chunk.data[11];
+			header.interlace = chunk.data[12];
+			
+			switch((PNGColour)header.colourType) {
+			case PNGColour::Lum:
+				if(header.bitDepth == 8) format = Image::R8;
+				else if(header.bitDepth == 16) format = Image::R16;
+				bitsPerPixel = header.bitDepth;
+				break;
+			case PNGColour::Indexed:
+				bitsPerPixel = header.bitDepth;
+				format = Image::RGB8;
+				break;
+			case PNGColour::LumAlpha: 
+				if(header.bitDepth == 8) format = Image::RG8;
+				bitsPerPixel = header.bitDepth * 2;
+				break;
+			case PNGColour::RGB:
+				if(header.bitDepth == 8) format = Image::RGB8;
+				bitsPerPixel = header.bitDepth * 3;
+				break;
+			case PNGColour::RGBA:
+				if(header.bitDepth == 8) format = Image::RGBA8;
+				bitsPerPixel = header.bitDepth * 4;
+				break;
+			}
+			if(format == Image::INVALID) break;
+			rowSize = header.width * bitsPerPixel/8;
+			rowBuffer = new byte[rowSize + 1];
+			pixels = new byte[rowSize * header.height];
+		}
+		else if(memcmp(chunk.type, "IDAT", 4)==0) { // Pixel data
+			if(stream.total_out) break; // Error - all IDAT blocks are handles together
+			bool fail = false;
+			stream.next_in = chunk.data;
+			stream.avail_in = chunk.length;
+
+			for(unsigned row = 0; row < header.height; ++row) {
+				stream.avail_out = rowSize+1;
+				stream.next_out = rowBuffer;
+
+				while(stream.avail_out) {
+					int r = inflate(&stream, Z_PARTIAL_FLUSH);
+
+					if(r==Z_BUF_ERROR && stream.avail_out > 0 && stream.avail_in==0) {
+						readChunk(chunk);
+						if(memcmp(chunk.type, "IDAT", 4) !=0 ) { fail=true; break; }
+						stream.next_in = chunk.data;
+						stream.avail_in = chunk.length;
+						continue;
+					}
+
+					if(r!=Z_OK && r!=Z_STREAM_END) {
+						printf("Inflate fail %d\n", r);
+						fail = true;
+						break;
+					}
+				}
+				if(fail) break;
+				
+				// Filter
+				switch(rowBuffer[0]) {
+				case 0: break;
+				case 1: readSubFilter(rowBuffer+1, rowSize, bitsPerPixel); break;
+				case 2: readUpFilter(rowBuffer+1, rowSize, pixels+(row-1)*rowSize); break;
+				case 3: readAvgFilter(rowBuffer+1, rowSize, bitsPerPixel, pixels+(row-1)*rowSize); break;
+				case 4: readPaethFilter(rowBuffer+1, rowSize, bitsPerPixel, pixels+(row-1)*rowSize); break;
+				default: printf("Bad filter %d\n", rowBuffer[0]); fail=true; break;
+				}
+				memcpy(pixels + rowSize * row, rowBuffer+1, rowSize);
+			}
+			if(fail) { format = Image::INVALID; break; }
+		}
+		else if(memcmp(chunk.type, "PLTE", 4)==0) { // Palette for indexed images
+			if(header.colourType == PNGColour::Indexed) {
+				palette = new byte[chunk.length];
+				memcpy(palette, chunk.data, chunk.length);
+			}
+		}
+		else if(memcmp(chunk.type, "IEND", 4)==0) {
+			break;
+		}
+		readChunk(chunk);
 	}
-	
-	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if(!png_ptr) return Image();
-	
-	png_infop info_ptr = png_create_info_struct(png_ptr);
-	if(!info_ptr) {
-		png_destroy_read_struct(&png_ptr, NULL, NULL);
-		return Image();
+	delete [] rowBuffer;
+	inflateEnd(&stream);
+
+	// Convert Indexed to RGB8 image
+	if(palette) {
+		unsigned count = header.width * header.height;
+		byte* index = pixels;
+		pixels = new byte[count * 3];
+		if(header.bitDepth == 8) {
+			for(unsigned i=0; i<count; ++i) memcpy(pixels+i*3, palette+index[i]*3, 3);
+		}
+		else {
+			byte mask = (1<<header.bitDepth) - 1;
+			unsigned orow = header.width * 3;
+			unsigned irow = (header.width * header.bitDepth + 7) >> 3;
+			for(unsigned y=0; y<header.height; ++y) {
+				byte* in = index + y * irow;
+				byte* out = pixels + y * orow;
+				byte* end = out + orow;
+				while(out < end) {
+					for(int s=8-header.bitDepth; s>=0; s-=header.bitDepth) {
+						int k = (*in >> s) & mask;
+						memcpy(out, palette+k*3, 3);
+						out += 3;
+					}
+					++in;
+				}
+			}
+		}
+		delete [] palette;
+		delete [] index;
 	}
-	
-	// initialize the setjmp for returning properly after a libpng error occured
-	if(setjmp(png_jmpbuf(png_ptr))) {
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		return Image();
-	}
-	
-	PNGMemoryReader source { (const char*)data, size, 8 };
-	png_set_read_fn(png_ptr, &source, png_read_memory);
-	return process_png_loader(png_ptr, info_ptr);
+
+	if(format != Image::INVALID) return Image(format, header.width, header.height, pixels);
+	delete [] pixels;
+	return Image();
 }
 
 
-Image PNG::load(const char* filename) {
-	FILE* fp = fopen(filename, "rb");
+Image PNG::load(const char* file) {
+	FILE* fp = fopen(file, "rb");
 	if(!fp) return Image();
 	
-	// check for valid magic number
-	png_byte magic[8];
-	fread(magic, 1, 8, fp);
-	if(!png_check_sig(magic, 8)) {
-		printf("Invalid PNG image\n"); 
-		fclose(fp);
-		return Image();
-	}
-	
-	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if(!png_ptr) {
-		fclose(fp);
-		return Image();
-	}
-	
-	png_infop info_ptr = png_create_info_struct(png_ptr);
-	if(!info_ptr) {
-		png_destroy_read_struct(&png_ptr, NULL, NULL);
-		fclose(fp);
-		return Image();
-	}
-	
-	// initialize the setjmp for returning properly after a libpng error occured
-	if(setjmp(png_jmpbuf(png_ptr))) {
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		fclose(fp);
-		return Image();
-	}
+	unsigned length = 1024;
+	byte* buffer = new byte[length];
+	byte intBuffer[4];
+	Image r = readPNGFile([fp, &buffer, &length, &intBuffer](unsigned len) {
+		if(feof(fp)) {
+			memset(buffer, 0, 4);
+			return buffer;
+		}
+		if(len==4) {
+			fread(intBuffer, 1, 4, fp);
+			return intBuffer;
+		}
+		if(len > length) {
+			delete [] buffer;
+			buffer = new byte[len];
+			length = len;
+		}
+		fread(buffer, 1, len, fp);
+		return buffer;
+	});
 
-	png_init_io (png_ptr, fp);
-
-	Image image = process_png_loader(png_ptr, info_ptr);
 	fclose(fp);
-	return image;
+	delete [] buffer;
+	if(!r) printf("Unsupported or invalid png image %s\n", file);
+	return r;
 }
 
-Image process_png_loader(png_structp png_ptr, png_infop info_ptr) {
-	png_set_sig_bytes(png_ptr, 8); // We have already read the signiture
-	
-	png_read_info(png_ptr, info_ptr);
-	int mcdepth = png_get_bit_depth(png_ptr, info_ptr);
-	int color_type = png_get_color_type(png_ptr, info_ptr);
-	if(color_type == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
-	
-	// convert 1-2-4 bits grayscale images to 8 bits grayscale.
-	int bit_depth = 32;
-	if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) png_set_expand_gray_1_2_4_to_8(png_ptr);
-	if(png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
-	if(bit_depth == 16) png_set_strip_16(png_ptr);
-	else if(bit_depth < 8) png_set_packing(png_ptr);
-	
-	/* update info structure to apply transformations */
-	png_read_update_info(png_ptr, info_ptr);
-	
-	// retrieve updated information
-	unsigned int mwidth;
-	unsigned int mheight;
-	png_get_IHDR(png_ptr, info_ptr,
-		(png_uint_32*)(&mwidth),
-		(png_uint_32*)(&mheight),
-		&mcdepth, &color_type,
-		NULL, NULL, NULL);
-	
-	// get image format and components per pixel
-	int iFormat=0;
-	Image::Format format;
-	switch (color_type) {
-	case PNG_COLOR_TYPE_GRAY:
-		if(bit_depth==8) {
-			iFormat = 1;
-			mcdepth = 8;
-			format = Image::R8;
-		}
-		else if(bit_depth == 16) {
-			format = Image::R16;
-			iFormat = 2;
-			mcdepth = 16;
-		}
-		break;
-	
-	case PNG_COLOR_TYPE_GRAY_ALPHA:
-		iFormat = 2;
-		mcdepth = 16;
-		format = Image::RG8;
-		break;
-	
-	case PNG_COLOR_TYPE_RGB:
-		iFormat = 3;
-		mcdepth = 24;
-		format = Image::RGB8;
-		break;
-	
-	case PNG_COLOR_TYPE_RGB_ALPHA:
-		iFormat = 4;
-		mcdepth = 32;
-		format = Image::RGBA8;
-		break;
-	
-	default: break;
-	}
-
-	if(iFormat == 0) {
-		printf("Invalid PNG Format\n");
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		return Image();
-	}
-
-	
-	// Read the image data
-	char* mdata = new char[mwidth * mheight * iFormat];
-	png_bytep* row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * mheight);
-	for(unsigned i=0; i < mheight; ++i) {
-		row_pointers[i] = (png_bytep)(mdata + (i * mwidth * iFormat));
-	}
-	png_read_image(png_ptr, row_pointers);
-	png_read_end(png_ptr, info_ptr);
-	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-	free(row_pointers);
-	
-
-	return Image(format, mwidth, mheight, (Image::byte*)mdata);
+Image PNG::parse(const char* data, unsigned size) {
+	const byte* stream = (const byte*)data;
+	const byte* end = stream + size;
+	return readPNGFile([&stream, end](unsigned len) {
+		const byte* r = stream;
+		stream += len;
+		if(stream > end) return end-len; // error - end of stream
+		return r;
+	});
 }
 
-bool PNG::save(const Image& image, const char* filename) {
+bool PNG::save(const base::Image& image, const char* file) {
 	if(!image) return false;
-	if(image.getMode() != Image::FLAT) return false;
-
-	FILE* fp = fopen(filename, "wb");
-	unsigned iFormat = image.getBytesPerPixel();
-	png_structp png_ptr;
-	png_infop info_ptr;
-	png_bytep *row_pointers = NULL;
+	if(image.getFormat() > Image::RGBA8) return false;
+	FILE* fp = fopen(file,"wb");
 	if(!fp) return false;
 
-	/** Initialise write struct */
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if(!png_ptr) {
-		fclose(fp);
-		return false;
-	}
-
-	/** Initialise info struct */
-	info_ptr = png_create_info_struct(png_ptr);
-	if(!info_ptr) {
-		png_destroy_write_struct(&png_ptr, NULL);
-		fclose(fp);
-		return false;
-	}
-	
-	/** Set up error handling */
-	if(setjmp( png_jmpbuf(png_ptr) )) {
-		png_destroy_write_struct(&png_ptr, NULL);
-		fclose(fp);
-		return false;
-	}
-
-	int format = 0;
-	switch(image.getFormat()) {
-	case Image::R8:    format = PNG_COLOR_TYPE_GRAY; break;
-	case Image::RG8:   format = PNG_COLOR_TYPE_GRAY_ALPHA; break;
-	case Image::RGB8:  format = PNG_COLOR_TYPE_RGB; break;
-	case Image::RGBA8: format = PNG_COLOR_TYPE_RGB_ALPHA; break;
-	default:
-		png_destroy_write_struct(&png_ptr, &info_ptr);
-		fclose(fp);
-		return false;
-	}
-
-	/** Set image attributes */
-	int width = image.getWidth();
-	int height = image.getHeight();
-	png_set_IHDR( png_ptr, info_ptr, width, height, 8, format,
-	              PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-	
-	/** Initialise rows */
-	row_pointers = (png_bytep*) malloc (sizeof (png_bytep) * height);
-	for(int i=0; i<height; ++i) {
-		row_pointers[i] = (png_bytep)(image.getData() + (i * width * iFormat));
-	}
-
-	/** Write image data */
-	png_init_io(png_ptr, fp);
-	png_set_rows(png_ptr, info_ptr, row_pointers);
-	png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-
-	/** cleanup */
-	free(row_pointers);
-	png_destroy_write_struct(&png_ptr, &info_ptr);
+	size_t length = 0;
+	void* data = tdefl_write_image_to_png_file_in_memory(image.getData(), image.getWidth(), image.getHeight(), image.getChannels(), &length);
+	fwrite(data, 1, length, fp);
 	fclose(fp);
+	mz_free(data);
 	return true;
 }
+
+// TODO:
+// - Write getBackground() function - return backgroundColour, or the first pixel (placeholder for BT stream)
+// 		dds loader needs this functionality too
+
 
