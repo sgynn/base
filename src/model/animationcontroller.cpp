9a6bc0d39a2c41b982dce954c1087be9641fb2e4
Sam Gynn <dragoninspire@gmail.com>
2025-04-08 16:06:59 +0100

    AnimationController adds random time offset to initial idle animation

--- src/model/animationcontroller.cpp
+++ src/model/animationcontroller.cpp
@@ -266,6 +266,7 @@ void AnimationController::setIdle(const AnimationKey& key) {
 		float initialWeight = m_idleTrack<0? 1: 0;
 		m_idleTrack = allocateActionTrack();
 		m_state->play(a->animation, 1, AnimationBlend::Add, initialWeight, true, -1, m_idleTrack);
+		if(initialWeight == 1) m_state->setFrameNormalised(randf());
 		setMeta(m_idleTrack, a, IDLE, ActionMode::Loop);
 		m_lastPosition += a->startPos;
 		m_lastOrientation *= a->startRot;
